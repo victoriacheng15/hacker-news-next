4eb0c96e5bef955ea0e98c42941c4651ca5fc051
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-14 13:48:40 -0700

    chore: add ApiProvider for apiSlice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "@/features/news/apisSlice";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ApiProvider api={apiSlice}>
+			<Component {...pageProps} />
+		</ApiProvider>
+	);
 }
