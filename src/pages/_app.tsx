ef4c5f453f0d10dc45c15a000335c84d5b9a6441
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-01-16 16:04:35 -0700

    Setup homepage (#15)
    * chore: populate homepage with placeholder
    
    * feat: add chakra provider and navbar
    
    * chore: clean up unused classes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "@/features/news/apisSlice";
+import Navbar from "@/components/Navbar";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ApiProvider api={apiSlice}>
-			<Component {...pageProps} />
+			<ChakraProvider>
+				<Navbar />
+				<Component {...pageProps} />
+			</ChakraProvider>
 		</ApiProvider>
 	);
 }
