ab7f32e5a6cd955db62b6555acc1f689adba7748
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-18 19:44:29 -0700

    chore: adjust display property for ul element instead of list element

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 function Logo() {
 	return (
-		<Text as="h1" fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold">
-			<Link href="/">Hacker News</Link>
-		</Text>
+		<Link href="/">
+			<Text as="h1" fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold">
+				Hacker News
+			</Text>
+		</Link>
 	);
 }
 
--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -4,33 +4,24 @@ import { Link, Text, ListItem } from "@chakra-ui/react";
 interface NavItemProps {
 	to: string;
 	link: string | React.ReactNode;
-	display?: string[] | string;
 	onClick?: () => void;
 }
 
-function NavItem({ to, link, display, onClick }: NavItemProps) {
+function NavItem({ to, link, onClick }: NavItemProps) {
 	return (
 		<>
-			{to === "/" ? (
-				<ListItem mr="auto">
-					<Link as={NextLink} href={to}>
+			<ListItem>
+				<Link as={NextLink} href={to} onClick={onClick}>
+					<Text
+						as="span"
+						fontSize="1.15rem"
+						fontWeight="medium"
+						textAlign="center"
+					>
 						{link}
-					</Link>
-				</ListItem>
-			) : (
-				<ListItem>
-					<Link as={NextLink} href={to} display={display} onClick={onClick}>
-						<Text
-							as="span"
-							fontSize="1.15rem"
-							fontWeight="medium"
-							textAlign="center"
-						>
-							{link}
-						</Text>
-					</Link>
-				</ListItem>
-			)}
+					</Text>
+				</Link>
+			</ListItem>
 		</>
 	);
 }
