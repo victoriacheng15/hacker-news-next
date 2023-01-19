8dd14a59d8ee4477598249c3cb30fd64394d2c0e
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-18 22:05:18 -0700

    feat: add Provider for store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { apiSlice } from "@/features/news/apisSlice";
-import Navbar from "@/components/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ApiProvider api={apiSlice}>
+		<Provider store={store}>
 			<ChakraProvider>
 				<Navbar />
 				<Component {...pageProps} />
 			</ChakraProvider>
-		</ApiProvider>
+		</Provider>
 	);
 }
