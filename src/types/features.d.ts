0b03490777d9c2a97c80f7fd65c768608936e4a6
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-20 21:55:18 -0700

    feat: add types for features folder

---
+++ src/types/features.d.ts
@@ -0,0 +1,30 @@
+interface ApiResponse {
+	details: Story[] | Job[];
+	status: "idle" | "loading" | "succeeded" | "failed";
+	error: boolean;
+	page: number;
+	limit: number;
+}
+
+type Pagination = Pick<ApiResponse, "page"| "limit">
+
+interface Story {
+	by: string,
+	id: number,
+	kids: number[],
+	score: number,
+	time: number,
+	title: string,
+	type: "story" | "job",
+	url: string
+}
+
+interface Job {
+	by: string,
+	id: number,
+	score: number,
+	time: number,
+	title: string,
+	type: "story" | "job",
+	url: string
+}
