03788c40f8e729661f810dafd1333522b755dd4e
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-20 21:55:41 -0700

    feat: add pages for top, show, and job

--- src/pages/show.tsx
+++ src/pages/show.tsx
@@ -1,5 +1,36 @@
+import { useEffect } from "react";
+import {
+	fetchShowDetails,
+	selectShows,
+} from "@/features/show/showStoriesSlice";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { loadMore } from "@/features/show/showStoriesSlice";
+
 function show() {
-	return <div>show</div>;
+	const dispatch = useAppDispatch();
+
+	const { details, status, error, page, limit } = useAppSelector(selectShows);
+
+	useEffect(() => {
+		if (status === "idle") {
+			dispatch(fetchShowDetails({ page, limit }));
+		}
+	}, [dispatch, status, page, limit]);
+
+	return (
+		<>
+			{error && <h2>something is wrong</h2>}
+			{details.map((story, index) => (
+				<div key={story.id}>
+					{index + 1} - {story.title} by {story.by}
+				</div>
+			))}
+			{status === "loading" && <h2>loading</h2>}
+			<button type="button" onClick={() => dispatch(loadMore())}>
+				Load more
+			</button>
+		</>
+	);
 }
 
 export default show;
