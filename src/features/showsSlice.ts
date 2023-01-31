c8af0f1fa7878e0ede94adfd2ee59e6d0b16bbad
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-30 17:08:03 -0700

    chore: comment-out slices for job and show

--- src/features/showsSlice.ts
+++ src/features/showsSlice.ts
@@ -1,42 +1,42 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import type { AppState } from "@/store";
-import { getAllDetails, initialState } from "./helpers";
+// import type { AppState } from "@/store";
+// import { getAllDetails, initialState } from "./helpers";
 
-export const fetchShowDetails = createAsyncThunk(
-	"shows/showsStoryDetails",
-	async ({ page, limit }: Pagination) => {
-		const details = await getAllDetails("show", page, limit);
-		return { details };
-	},
-);
+// export const fetchShowDetails = createAsyncThunk(
+// 	"shows/showsStoryDetails",
+// 	async ({ page, limit }: Pagination) => {
+// 		const details = await getAllDetails("show", page, limit);
+// 		return { details };
+// 	},
+// );
 
-const showsSlice = createSlice({
-	name: "shows",
-	initialState,
-	reducers: {
-		loadMore: (state) => {
-			state.status = "idle";
-			state.limit += 10;
-		},
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(fetchShowDetails.pending, (state) => {
-				state.status = "loading";
-			})
-			.addCase(fetchShowDetails.fulfilled, (state, action) => {
-				state.status = "succeeded";
-				state.details = action.payload.details;
-			})
-			.addCase(fetchShowDetails.rejected, (state) => {
-				state.status = "failed";
-				state.error = true;
-			});
-	},
-});
+// const showsSlice = createSlice({
+// 	name: "shows",
+// 	initialState,
+// 	reducers: {
+// 		loadMore: (state) => {
+// 			state.status = "idle";
+// 			state.limit += 10;
+// 		},
+// 	},
+// 	extraReducers: (builder) => {
+// 		builder
+// 			.addCase(fetchShowDetails.pending, (state) => {
+// 				state.status = "loading";
+// 			})
+// 			.addCase(fetchShowDetails.fulfilled, (state, action) => {
+// 				state.status = "succeeded";
+// 				state.details = action.payload.details;
+// 			})
+// 			.addCase(fetchShowDetails.rejected, (state) => {
+// 				state.status = "failed";
+// 				state.error = true;
+// 			});
+// 	},
+// });
 
-export const { loadMore } = showsSlice.actions;
+// export const { loadMore } = showsSlice.actions;
 
-export const selectShows = (state: AppState) => state.shows;
+// export const selectShows = (state: AppState) => state.shows;
 
-export default showsSlice;
+// export default showsSlice;
