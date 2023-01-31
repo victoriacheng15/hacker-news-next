150d8be30572262ec1a69c8835740f32822c561a
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-30 17:05:06 -0700

    chore: comment-out existed code

--- src/pages/job.tsx
+++ src/pages/job.tsx
@@ -1,22 +1,24 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/hooks";
-import { fetchJobDetails, selectJobs } from "@/features/jobsSlice";
-import { loadMore } from "@/features/jobsSlice";
+import MainContainer from "@/components/MainContainer";
+// import { fetchJobDetails, selectJobs } from "@/features/jobsSlice";
+// import { loadMore } from "@/features/jobsSlice";
 
 function job() {
 	const dispatch = useAppDispatch();
 
-	const { details, status, error, page, limit } = useAppSelector(selectJobs);
+	// const { details, status, error, page, limit } = useAppSelector(selectJobs);
 
-	useEffect(() => {
-		if (status === "idle") {
-			dispatch(fetchJobDetails({ page, limit }));
-		}
-	}, [dispatch, status, page, limit]);
+	// useEffect(() => {
+	// 	if (status === "idle") {
+	// 		dispatch(fetchJobDetails({ page, limit }));
+	// 	}
+	// }, [dispatch, status, page, limit]);
 
 	return (
-		<>
-			{error && <h2>something is wrong</h2>}
+		<MainContainer>
+      <h2>job story</h2>
+			{/* {error && <h2>something is wrong</h2>}
 			{details.map((story, index) => (
 				<div key={story.id}>
 					{index + 1} - {story.title} by {story.author}
@@ -25,8 +27,8 @@ function job() {
 			{status === "loading" && <h2>loading</h2>}
 			<button type="button" onClick={() => dispatch(loadMore())}>
 				Load more
-			</button>
-		</>
+			</button> */}
+		</MainContainer>
 	);
 }
 
--- src/pages/show.tsx
+++ src/pages/show.tsx
@@ -1,27 +1,28 @@
-/* eslint-disable react/no-children-prop */
+// /* eslint-disable react/no-children-prop */
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/hooks";
-import { fetchShowDetails, selectShows } from "@/features/showsSlice";
-import { loadMore } from "@/features/showsSlice";
+// import { fetchShowDetails, selectShows } from "@/features/showsSlice";
+// import { loadMore } from "@/features/showsSlice";
 import MainContainer from "@/components/MainContainer";
 import LoadMoreBtn from "@/components/LoadMoreBtn";
 import LoadingInfo from "@/components/LoadingInfo";
-import Story from "@/components/Story";
+// import Story from "@/components/Story";
 
 function show() {
 	const dispatch = useAppDispatch();
 
-	const { details, status, error, page, limit } = useAppSelector(selectShows);
+	// const { details, status, error, page, limit } = useAppSelector(selectShows);
 
-	useEffect(() => {
-		if (status === "idle") {
-			dispatch(fetchShowDetails({ page, limit }));
-		}
-	}, [dispatch, status, page, limit]);
+	// useEffect(() => {
+	// 	if (status === "idle") {
+	// 		dispatch(fetchShowDetails({ page, limit }));
+	// 	}
+	// }, [dispatch, status, page, limit]);
 
 	return (
 		<MainContainer>
-			{error && <h2>something is wrong</h2>}
+      <h2>Show stories</h2>
+			{/* {error && <h2>something is wrong</h2>}
 			{(details as Story[]).map(
 				({ id, title, author, children, url, text, created_at, points }) => (
 					<Story
@@ -40,7 +41,7 @@ function show() {
 			<LoadingInfo status={status} error={error} />
 			{status === "succeeded" && (
 				<LoadMoreBtn onClick={() => dispatch(loadMore())} />
-			)}
+			)} */}
 		</MainContainer>
 	);
 }
