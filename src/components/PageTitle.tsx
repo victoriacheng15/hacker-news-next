dbe48fc4e50c1ea74e1718813e2ecae2bf872fc1
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-30 17:05:41 -0700

    chore: increase more margin for the page title

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 
 function PageTitle({ pageTitle }: { pageTitle: string }) {
 	return (
-		<Heading color="orange.700" mb="4">
+		<Heading color="orange.700" mb="6">
 			{pageTitle}
 		</Heading>
 	);
