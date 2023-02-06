81e63ef331b8fadc8f82e164f1975f29a0a955d2
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-06 15:34:06 -0700

    refactor: add Button to story for modal feature

--- src/components/StoryBlock/index.tsx
+++ src/components/StoryBlock/index.tsx
@@ -1,27 +1,62 @@
-import { Flex, Text, Divider, Heading } from "@chakra-ui/react";
+import {
+	Flex,
+	Text,
+	Divider,
+	Heading,
+	Modal,
+	ModalOverlay,
+	useDisclosure,
+	Button,
+} from "@chakra-ui/react";
 import { IItem } from "hacker-news-api-types";
-import { timeAgo } from "@/utils/timeFormater";
+import { useAppDispatch } from "@/hooks";
+import { fetchTopComments } from "@/features/topsSlice";
+import { timeAgo } from "@/utils/timeAgo";
+import StoryCommentModal from "../StoryCommentModal";
 
-function StoryBlock({ title, by, time, score, descendants }: IItem) {
+function StoryBlock({ title, by, time, score, descendants, id }: IItem) {
+	const dispatch = useAppDispatch();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	function modalAndDispatch() {
+		onOpen();
+		dispatch(fetchTopComments(id));
+	}
+
 	return (
-		<Flex
-			as="section"
-			border="1px"
-			borderColor="orange.700"
-			borderRadius="md"
-			p="2"
-			flexDir="column"
-			gap="2"
+		<Button
+			h="full"
+			w="full"
+			cursor="pointer"
+			border="2px"
+			bg="gray.100"
+			borderColor="orange.300"
+			aria-label="open modal"
+			onClick={modalAndDispatch}
 		>
-			<Heading as="h3" fontSize="2xl">
-				{title}
-			</Heading>
-			<Divider />
-			<Text>
-				{score} points | by: {by} | Published {timeAgo(time as number)} ago |{" "}
-				{descendants} comments
-			</Text>
-		</Flex>
+			<Flex as="section" w="full" p="3" flexDir="column" gap="2" data-id={id}>
+				<Heading as="h3" textAlign="left" fontSize="2xl">
+					{title}
+				</Heading>
+				<Divider />
+				<Flex>
+					<Text>
+						{score} points | by: {by} | Published {timeAgo(time as number)} ago
+						| {descendants} comments
+					</Text>
+					<Modal
+						size="6xl"
+						blockScrollOnMount={true}
+						isOpen={isOpen}
+						onClose={onClose}
+						motionPreset="slideInBottom"
+					>
+						<ModalOverlay />
+						<StoryCommentModal id={id} />
+					</Modal>
+				</Flex>
+			</Flex>
+		</Button>
 	);
 }
 
