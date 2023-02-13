83ba7adea5cd8aa031d8a51dd0df0e5c8d8eaa6b
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-13 16:20:25 -0700

    chore: populate shows page ui

--- src/pages/show.tsx
+++ src/pages/show.tsx
@@ -1,47 +1,47 @@
 // /* eslint-disable react/no-children-prop */
 import { useEffect } from "react";
+import { Flex } from "@chakra-ui/react";
 import { useAppDispatch, useAppSelector } from "@/hooks";
-// import { fetchShowDetails, selectShows } from "@/features/showsSlice";
-// import { loadMore } from "@/features/showsSlice";
+import { fetchShowStories, selectShows } from "@/features/showsSlice";
+import { loadMoreStories } from "@/features/showsSlice";
 import MainContainer from "@/components/MainContainer";
+import PageTitle from "@/components/PageTitle";
+import StoryBlock from "@/components/StoryBlock";
 import LoadMoreBtn from "@/components/LoadMoreBtn";
 import LoadingInfo from "@/components/LoadingInfo";
-// import Story from "@/components/Story";
 
 function show() {
 	const dispatch = useAppDispatch();
+	const { details, loadingStatus, error, page, limit } =
+		useAppSelector(selectShows);
 
-	// const { details, status, error, page, limit } = useAppSelector(selectShows);
-
-	// useEffect(() => {
-	// 	if (status === "idle") {
-	// 		dispatch(fetchShowDetails({ page, limit }));
-	// 	}
-	// }, [dispatch, status, page, limit]);
+	useEffect(() => {
+		if (loadingStatus === "idle") {
+			dispatch(fetchShowStories({ page, limit }));
+		}
+	}, [dispatch, loadingStatus, page, limit]);
 
 	return (
 		<MainContainer>
-			<h2>Show stories</h2>
-			{/* {error && <h2>something is wrong</h2>}
-			{(details as Story[]).map(
-				({ id, title, author, children, url, text, created_at, points }) => (
-					<Story
-						key={id}
-						id={id}
-						title={title}
-						author={author}
-						children={children}
-						url={url}
-						text={text}
-						created_at={created_at}
-						points={points}
+			<PageTitle pageTitle="Show Stories" />
+			<Flex flexDir="column" gap="6">
+				{details.map((detail) => (
+					<StoryBlock
+						key={detail.id}
+						id={detail.id}
+						title={detail.title}
+						by={detail.by}
+						time={detail.time}
+						score={detail.score}
+						descendants={detail.descendants}
 					/>
-				),
-			)}
-			<LoadingInfo status={status} error={error} />
-			{status === "succeeded" && (
-				<LoadMoreBtn onClick={() => dispatch(loadMore())} />
-			)} */}
+				))}
+			</Flex>
+			<LoadingInfo status={loadingStatus} error={error} />
+			<LoadMoreBtn
+				btnText="Load More Stories!"
+				onClick={() => dispatch(loadMoreStories())}
+			/>
 		</MainContainer>
 	);
 }
