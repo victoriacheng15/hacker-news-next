53761f3589ffbd34f860915789231f808cfdd275
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-16 14:40:15 -0700

    Edit hover style (#43)
    * feat: add hover style for LoadMoreBtn
    
    * feat: add hover style for navbar links
    
    * feat: add hover style for StoryBlock component
    
    * feat: add hover style for view more button and make bg darker

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,14 +1,31 @@
-import { Text } from "@chakra-ui/react";
-import Link from "next/link";
+import { Link, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 function Logo() {
-	return (
-		<Link href="/">
-			<Text as="h1" fontSize={["2xl", "3xl", "4xl"]} fontWeight="bold">
-				Hacker News
-			</Text>
-		</Link>
-	);
+  return (
+    <Link
+      as={NextLink}
+      href="/"
+      _hover={{
+        textDecoration: "none",
+      }}
+    >
+      <Text
+        as="h1"
+        fontSize={["2xl", "3xl", "4xl"]}
+        fontWeight="bold"
+				py="2"
+				px="3"
+        _hover={{
+					color: "orange.100",
+					borderRadius: "lg",
+					textDecoration: "wavy",
+        }}
+      >
+        Hacker News
+      </Text>
+    </Link>
+  );
 }
 
 export default Logo;
