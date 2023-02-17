9972a2a1708f1cb7a9ab970a3e393aa37fb67e04
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-17 16:07:32 -0700

    feat: add useFetchTop to handle fetch top stories

---
+++ src/hooks/useFetchTop.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useFetchAllStories } from "./useFetchAllStories";
+
+export function useFetchTop() {
+	const {
+		tops,
+		topLoading,
+		topError,
+		topPage,
+		topLimit,
+		dispatchTopStories,
+		dispatchMoreTop
+	} = useFetchAllStories();
+
+	useEffect(() => {
+		if (topLoading === "idle") {
+			dispatchTopStories();
+		}
+	}, [dispatchTopStories, topLoading, topPage, topLimit]);
+
+	return {
+		tops,
+		topLoading,
+		topError,
+		dispatchMoreTop,
+	};
+}
