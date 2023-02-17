029f20608afa14d373ae9fca29bfd7f84b981cbe
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-17 13:43:23 -0700

    feat: add TextStatsProps, RecentBlockProps and LoaMoreBtnProps

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,16 +1,35 @@
 type LoadingInfoProps = Pick<ApiResponse, "status" | "error">;
 
 interface Comments {
-	id: number;
-	created_at_i: number;
-	author: string;
-	text: string;
-	title: string;
-	url: string;
-	children: Comments[];
+  id: number;
+  created_at_i: number;
+  author: string;
+  text: string;
+  title: string;
+  url: string;
+  children: Comments[];
 }
 
 type CommentProps = Pick<
-	StoryComments,
-	"author" | "time" | "text" | "children" | "create_at"
+  StoryComments,
+  "author" | "time" | "text" | "children" | "create_at"
 >;
+
+interface TextStatsProps {
+  by?: string;
+  score?: number;
+  time?: number;
+  descendants?: number;
+  type?: string;
+}
+
+interface RecentBlockProps {
+  stories: IItem[];
+  href: string;
+}
+
+
+interface LoadMoreBtnProps {
+  btnText: string; 
+  onClick: () => void 
+}
