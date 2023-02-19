1d1dd26ec906121889eb56aa00a8dc6ef96d2a29
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-19 11:26:52 -0700

    feat: add commentsSlice

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import topsSlice from "@/features/topsSlice";
 import showsSlice from "@/features/showsSlice";
 import jobsSlice from "@/features/jobsSlice";
+import commentsSlice from "./features/commentsSlice";
 
 export const store = configureStore({
 	reducer: {
 		[topsSlice.name]: topsSlice.reducer,
 		[showsSlice.name]: showsSlice.reducer,
 		[jobsSlice.name]: jobsSlice.reducer,
+		[commentsSlice.name]: commentsSlice.reducer,
 	},
 });
 
