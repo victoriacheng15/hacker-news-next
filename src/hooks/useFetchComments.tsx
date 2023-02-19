1069775d2d8b921632b6f2171c02c77e19c1d076
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-19 12:18:23 -0700

    feat: add useFetchComments hook

---
+++ src/hooks/useFetchComments.tsx
@@ -0,0 +1,23 @@
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { selectComments } from "@/features/commentsSlice";
+import { fetchComments } from "@/features/commentsSlice";
+import { loadMoreComments } from "@/features/commentsSlice";
+
+export function useFetchComments() {
+	const dispatch = useAppDispatch();
+
+	const { comments, commentLoading, commentPage, commentLimit } =
+		useAppSelector(selectComments);
+
+  const dispatchFetchComments  = (id: number) => dispatch(fetchComments(id));
+  const dispatchLoadMoreComments = () => dispatch(loadMoreComments());
+
+  return {
+    comments,
+    commentLoading,
+    commentPage,
+    commentLimit,
+    dispatchFetchComments,
+    dispatchLoadMoreComments,
+  }
+}
