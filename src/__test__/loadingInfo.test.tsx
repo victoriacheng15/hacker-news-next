cdb80fa9c52f82e786f4080dbe015e70698cd4a5
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-22 09:33:23 -0700

    chore: change to queryBy method for loadingInfo.test.tsx

--- src/__test__/loadingInfo.test.tsx
+++ src/__test__/loadingInfo.test.tsx
@@ -4,15 +4,23 @@ import LoadingInfo from "../components/LoadingInfo";
 
 
 describe("LoadingInfo component", () => {
-  it("renders spinner component when status equals to loading", async () => {
+  it("should not render spinner component when status equals to idle", () => {
+    render(<LoadingInfo status="idle" error="" />);
+    const spinner = screen.queryByTestId("spinner");
+    expect(spinner).not.toBeInTheDocument();
+  })
+
+  it("renders spinner component when status equals to loading", () => {
     render(<LoadingInfo status="loading" error="" />);
-    const spinner = await screen.findByTestId("spinner");
+    const spinner = screen.queryByTestId("spinner");
     expect(spinner).toBeInTheDocument();
   });
 
+
+
   it("renders error message when error is not an empty string", async () => {
     render(<LoadingInfo status="failed" error="cant fetch request" />);
-    const errorMessage = await screen.findByRole("heading");
+    const errorMessage = await screen.queryByRole("heading");
     expect(errorMessage).toHaveTextContent("something is wrong");
   });
 })
