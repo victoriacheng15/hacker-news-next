686521d4519ff7d5c4ea795d6390436e9c34ad7d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-07-23 10:30:11 -0600

    bump packages (#64)
    * bump: update packages to latest
    
    * chore: remove plugins option

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	test: {
-    globals: true,
+		globals: true,
 		environment: "jsdom",
-    setupFiles: "./src/setup.ts",
+		setupFiles: "./src/setup.ts",
 	},
 });
